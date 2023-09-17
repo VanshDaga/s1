30d00bef160e4db9a1efd8d2cbe5af16c5649f4a
VanshDaga <107114604+VanshDaga@users.noreply.github.com>
2023-09-17 11:03:21 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+
+var bgimg
+
+function preload(){
+
+bgimg=loadImage("")
+   "Big Wings!!"
+}
+
+function setup(){
+createCanvas(windowWidth,windowHeight)
+
+
+}
+
+function draw(){
+background(bgimg)
+
+
+
+}
